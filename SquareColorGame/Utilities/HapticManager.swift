83f84a9b8d95d09d1ac96cac0a7804731827e91c
Malith Kumaranayaka <127102703+malith45@users.noreply.github.com>
2026-01-23 15:30:14 +0530

    Updated HapticManager.swift to implement haptic feedback methods.

--- SquareColorGame/Utilities/HapticManager.swift
+++ SquareColorGame/Utilities/HapticManager.swift
@@ -1,1 +1,45 @@
-//\n//  HapticManager.swift\n//  SquareColorGame\n//\n//  Created on 2026-01-17\n//\n\nimport UIKit\n\nclass HapticManager {\n    static let shared = HapticManager()\n    \n    private init() {}\n    \n    func impact(_ style: UIImpactFeedbackGenerator.FeedbackStyle) {\n        let generator = UIImpactFeedbackGenerator(style: style)\n        generator.prepare()\n        generator.impactOccurred()\n    }\n    \n    func notification(_ type: UINotificationFeedbackGenerator.FeedbackType) {\n        let generator = UINotificationFeedbackGenerator()\n        generator.prepare()\n        generator.notificationOccurred(type)\n    }\n    \n    func selection() {\n        let generator = UISelectionFeedbackGenerator()\n        generator.prepare()\n        generator.selectionChanged()\n    }\n    \n    // Convenience methods\n    func success() {\n        notification(.success)\n    }\n    \n    func error() {\n        notification(.error)\n    }\n    \n    func warning() {\n        notification(.warning)\n    }\n}
+//
+//  HapticManager.swift
+//  SquareColorGame
+//
+//  Created on 2026-01-17
+//
+
+import UIKit
+
+class HapticManager {
+    static let shared = HapticManager()
+    
+    private init() {}
+    
+    func impact(_ style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.prepare()
+        generator.impactOccurred()
+    }
+    
+    func notification(_ type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.prepare()
+        generator.notificationOccurred(type)
+    }
+    
+    func selection() {
+        let generator = UISelectionFeedbackGenerator()
+        generator.prepare()
+        generator.selectionChanged()
+    }
+    
+    // Convenience methods
+    func success() {
+        notification(.success)
+    }
+    
+    func error() {
+        notification(.error)
+    }
+    
+    func warning() {
+        notification(.warning)
+    }
+}
